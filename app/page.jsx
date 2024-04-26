b96a158b3db784c6ff7a4f07aa9fa7d84a6936eb
destinyoo <ismailabdelkader0@gmail.com>
2024-04-26 15:16:07 +0100

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import TicketCard from "./(components)/TicketCard";
+import PriorityDisplay from "./(components)/PriorityDisplay";
+const Dashboard = () => {
+  return (
+    <div>
+      <TicketCard />
+      <TicketCard />
+      <TicketCard />
+    </div>
+  );
+};
+
+export default Dashboard;
